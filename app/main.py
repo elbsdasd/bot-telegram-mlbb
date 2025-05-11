8194e30c192c0c3f9fa188acb896ecd6651d1f9d
BenjaminVenturaTorres <benjaminventura415@gmail.com>
2025-05-11 13:58:11 -0500

    Actualización de main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
+# app/main.py
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 import os
 from dotenv import load_dotenv
-import logging
+
+from app.handlers import (
+    say_hello,
+    show_menu,
+    handle_buttons,
+    send_video_tutorial,
+    send_info_texturas
+)
 
 # Cargar las variables de entorno
 load_dotenv()
-
-# Configurar el logging de la aplicación
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # Obtener el TOKEN de Telegram desde las variables de entorno
 TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -16,10 +20,8 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 # Comprobación para asegurar que las variables de entorno están configuradas correctamente
 if not TOKEN:
-    logger.error("La variable de entorno 'TELEGRAM_TOKEN' no está configurada correctamente.")
     raise ValueError("La variable de entorno 'TELEGRAM_TOKEN' no está configurada correctamente.")
 if not WEBHOOK_URL:
-    logger.error("La variable de entorno 'WEBHOOK_URL' no está configurada correctamente.")
     raise ValueError("La variable de entorno 'WEBHOOK_URL' no está configurada correctamente.")
 
 def main():
@@ -33,14 +35,13 @@ def main():
     application.add_handler(CommandHandler("Inf_Texturas", send_info_texturas))
     application.add_handler(CallbackQueryHandler(handle_buttons))
 
-    # Configurar el webhook
-    logger.info("Iniciando el servidor con el webhook en %s", WEBHOOK_URL)
+    # Configurar el webhook para Heroku
     application.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 5000)),  # El puerto puede estar configurado en Heroku como variable de entorno
-        webhook_url=WEBHOOK_URL  # La URL, evitando imprimir el token
+        webhook_url=WEBHOOK_URL  # La URL de tu webhook
     )
-    
+
 if __name__ == "__main__":
     main()
 
